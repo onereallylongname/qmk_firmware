e00fd6a42928f19b036bac16ce7a3f9cf38aebd7
onereallylongname <antonioalmeida2099@gmail.com>
2023-02-09 22:15:21 +0000

    WIP review the layout.

--- keyboards/lily58/keymaps/onereallylongname/keymap.c
+++ keyboards/lily58/keymaps/onereallylongname/keymap.c
@@ -5,7 +5,7 @@ enum layer_number {
   _QWERTY = 0,
   _SYMBOL,
   _MOVE,
-  _ADJUST,
+  _CTRL,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  ´   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ç  |  ~   |
- * |------+------+------+------+------+------|QUERTY |    | RGUI  |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| code  |    | APP   |------+------+------+------+------+------|
  * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  |RSHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |SYMBOL| /Space  /       \Enter \  | CTRL |BackSP| MOVE |
@@ -27,42 +27,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    PT_ACUT,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    PT_CCED, PT_TILD,
-  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,TO(_QWERTY), KC_APP, KC_N,    KC_M,    PT_COMM, PT_DOT,  PT_MINS, KC_RSFT,
-                       KC_LALT, KC_LGUI, MO(_SYMBOL),KC_SPACE,   KC_ENT, KC_RCTL, KC_BSPC,TG(_MOVE)
+  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, RWIN(KC_1), KC_APP, KC_N,    KC_M,    PT_COMM, PT_DOT,  PT_MINS, KC_RSFT,
+                       KC_LALT, KC_LGUI, MO(_SYMBOL), KC_SPACE,  KC_ENT, KC_RCTL, KC_BSPC,TO(_MOVE)
  ),
 
 /* SYMBOL
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |  5   |                    |   6  |   7  |   8  |   9  |   0  | DEL  |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | CAPS |  !   |  "   |  #   |  $   |  %   |                    |  &   |  /   |   (  |   )  |  =   |  '   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|  |   |  @   |  <   |  >   |  \   |-------.    ,-------|  ^   |   &  |   {  |   }  |  +   |  *   |
+ * | LAlt |  |   |  @   |  \   |  <   |  >   |-------.    ,-------|  ^   |   &  |   {  |   }  |  *   |  +   |
  * |------+------+------+------+------+------| QUERTY|    | RGUI  |------+------+------+------+------+------|
- * |LCTRL |  º   |  €   |  £   |  §   |  ¨   |-------|    |-------|  ;   |   :  |   [  |   ]  |  ?   |RSHIFT|
+ * |LCTRL |  º   |  ª   |  €   |  §   |  £   |-------|    |-------|  ;   |   :  |   [  |   ]  |  ?   |RSHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |SYMBOL| /Space  /       \Enter \  | CTRL |BackSP| MOVE |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_SYMBOL] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
-  KC_CAPS, PT_EXLM, PT_DQUO, PT_HASH, PT_DLR , PT_PERC,                   PT_AMPR, PT_SLSH, PT_LPRN, PT_RPRN, PT_EQL , KC_F12,
-  _______, PT_PIPE, PT_AT  , PT_LABK, PT_RABK, PT_BSLS,                   PT_CIRC, PT_AMPR, PT_LCBR, PT_RCBR, PT_PLUS, PT_ASTR,
-  _______, PT_MORD, PT_EURO, PT_PND , PT_SECT, PT_DIAE, _______, _______, PT_SCLN, PT_COLN, PT_LBRC, PT_RBRC, PT_QUES, KC_RSFT,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+  KC_F1  ,  KC_F2 ,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                  KC_F7  ,  KC_F8 , KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  KC_CAPS, PT_EXLM, PT_DQUO, PT_HASH, PT_DLR , PT_PERC,                  PT_AMPR, PT_SLSH, PT_LPRN, PT_RPRN, PT_EQL , PT_QUOT,
+  KC_LALT, PT_PIPE, PT_AT  , KC_GRV,  PT_LABK, PT_RABK,                  PT_CIRC, PT_AMPR, PT_LCBR, PT_RCBR, PT_ASTR, PT_PLUS,
+  _______, PT_MORD, PT_FORD, PT_EURO, PT_SECT, PT_PND, _______, _______, PT_SCLN, PT_COLN, PT_LBRC, PT_RBRC, PT_QUES, KC_RSFT,
+                             _______, _______, _______, _______, _______,  _______, _______, XXXXXXX
 ),
 /* MOVE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
- * |-------+------+------+------+------+-----|                    |------+------+------+------+------+------|
- * | TAB  | BRIU | VOLU | MPLY | MPRV |      |                    |      | HOME | END  | DEL  | PSCR |      |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | DEL  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LSHIFT| BRID | VOLD | MUTE | MNXT |      |-------.    ,-------| Left | Down |  Up  |Right |      | INS  |
+ * | TAB  |   Q  |   W  |   E  |   R  |   T  |                    |      | HOME | END  | DEL  | ESC  | PSCR |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LSHIFT|   A  |   S  |   D  |   F  |   G  |-------.    ,-------| Left | Down |  Up  |Right |      | INS  |
  * |------+------+------+------+------+------| QUERTY|    | RGUI  |------+------+------+------+------+------|
- * |LCTRL | KC_Z | KC_X | KC_C | KC_V | KC_B |-------|    |-------|      | PGDN | PGDU |      | ESC  |RSHIFT|
+ * |LCTRL |  Z   |  X   |  C   |  V   |  B   |-------|    |-------|      | PGDN | PGDU |      | ESC  |RSHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |SYMBOL| /Space  /       \Enter \  | CTRL |BackSP| MOVE |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -70,39 +70,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_MOVE] = LAYOUT(
-  KC_F1  ,  KC_F2  ,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7  ,  KC_F8 , KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_TAB ,  KC_BRIU,   KC_VOLU, KC_MPLY, KC_MPRV, XXXXXXX,                      XXXXXXX, KC_HOME, KC_END,  KC_DEL,  KC_ESC,  KC_PSCR,
-  _______,  KC_BRID,   KC_VOLD, KC_MUTE, KC_MNXT, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_INS ,
-  _______,  KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,   _______,    _______,  XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, KC_ESC, _______,
-                             _______, _______, _______,  _______,       _______,  _______, _______, _______
+  KC_ESC,   KC_1,      KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_TAB ,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                            XXXXXXX, KC_HOME, KC_END,  KC_DEL,  KC_ESC,  KC_PSCR,
+  _______,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_INS ,
+  _______,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______,     _______,  XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, KC_ESC, _______,
+                             _______, _______, _______,  _______,       _______,  _______, _______, XXXXXXX
 ),
 
-/* ADJUST
+/* CTRL
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      | BRIU | VOLU | MPLY | MPRV |      |                    |  F13 |  F14 |  F15 | F16  | F17  | F18  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      |      |RGB ON| HUE+ | SAT+ | VAL+ |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
+ * | LAlt | BRID | VOLD | MUTE | MNXT |      |-------.    ,-------|      |RGB ON| HUE+ | SAT+ | VAL+ |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+-------+-----|
+ * |      |      |      |      |      |      |-------|    |-------|      | MODE | HUE- | SAT- | VAL- |RSHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |SYMBOL| /Space  /       \Enter \  | CTRL |BackSP| MOVE |
+ *                   | LAlt | LGUI |SYMBOL| /Space  /       \Enter \  | CTRL |BackSP|QWERTY|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
-[_ADJUST] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+[_CTRL] = LAYOUT(
+  KC_F1  ,  KC_F2 ,   KC_F3,   KC_F4,   KC_F5,  KC_F6,                    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  XXXXXXX, KC_BRIU, KC_VOLU, KC_MPLY, KC_MPRV, XXXXXXX,                   KC_F13   KC_F14   KC_F15,  KC_F16,  KC_F17,  KC_F18,
+  KC_LALT, KC_BRID, KC_VOLD, KC_MUTE, KC_MNXT, XXXXXXX,                   XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,
+                             _______, _______, _______, _______, _______,  _______, _______, TO(_QUERTY)
 )
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _SYMBOL, _MOVE, _ADJUST);
+  return update_tri_layer_state(state, _SYMBOL, _MOVE, _CTRL);
 }
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
@@ -114,6 +114,39 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   return rotation;
 }
 
+void print_top_animation(uint8_t frameNumber, led_t led_state) {
+
+    switch (frameNumber) {
+        case 0:
+            oled_write_P(PSTR(" "), false);
+            break;
+        case 1:
+            oled_write_P(PSTR("   "), false);
+            break;
+        case 2:
+            oled_write_P(PSTR("     "), false);
+            break;
+        case 3:
+            oled_write_P(PSTR("       "), false);
+            break;
+        case 4:
+            oled_write_P(PSTR("         "), false);
+            break;
+         case 5:
+            oled_write_P(PSTR("           "), false);
+            break;
+         case 6:
+            oled_write_P(PSTR("             "), false);
+            break;
+       default:
+            oled_write_P(PSTR(" "), false);
+    }
+
+    oled_write_ln_P(led_state.caps_lock
+            ? PSTR("=////=")
+            : PSTR("=\\\\\\\\="), false);
+}
+
 // When you add source files to SRC in rules.mk, you can use functions.
 const char *read_layer_state(void);
 const char *read_logo(void);
@@ -121,8 +154,9 @@ void set_keylog(uint16_t keycode, keyrecord_t *record);
 const char *read_keylog(void);
 const char *read_keylogs(void);
 // Animation parameters
-#define FRAME_DURATION 1500// How long each frame lasts in milliseconds
-#define MAX_FRAME_POS 4
+#define FRAME_DURATION 800// How long each frame lasts in milliseconds
+#define POINTER_FRAME_DURATION 600// How long each pointer frame lasts in milliseconds
+#define MAX_FRAME_POS 6
 // Animation variables
 uint32_t timer = 0;
 uint8_t current_frame = 0;
@@ -135,9 +169,9 @@ int8_t current_frame_inc = 1;
 
 bool oled_task_user(void) {
   if (is_keyboard_master()) {
-
+    uint16_t time_since_last_frame = timer_elapsed(timer);
     // Run animation
-    if (timer_elapsed(timer) > FRAME_DURATION) {
+    if ( time_since_last_frame > FRAME_DURATION) {
         // Set timer to updated time
         timer = timer_read();
 
@@ -150,51 +184,90 @@ bool oled_task_user(void) {
         current_frame = (current_frame + current_frame_inc);
     }
 
-    switch (current_frame) {
-        case 0:
-            oled_write_ln_P(PSTR(" =////="), false);
-            break;
-        case 1:
-            oled_write_ln_P(PSTR("   =////="), false);
-            break;
-        case 2:
-            oled_write_ln_P(PSTR("     =////="), false);
-            break;
-        case 3:
-            oled_write_ln_P(PSTR("       =////="), false);
-            break;
-        case 4:
-            oled_write_ln_P(PSTR("         =////="), false);
-            break;
-        default:
-            oled_write_ln_P(PSTR(" =////="), false);
-    }
-
     led_t led_state = host_keyboard_led_state();
-    oled_write_P(PSTR("layer: "), false);
+    print_top_animation(current_frame, led_state);
+
+    // oled_write_P(PSTR("layer: "), false);
     // Switch on current active layer
     switch (get_highest_layer(layer_state)) {
         case _QWERTY :
-            oled_write_ln_P(led_state.caps_lock ? PSTR(" QUERTY") : PSTR("querty"), false);
+            if ( time_since_last_frame > POINTER_FRAME_DURATION)
+            {
+              oled_write_ln_P(led_state.caps_lock
+              ? PSTR(" >QWERTY<   SYMBOL")
+              : PSTR(" >qwerty<   symbol"), false);
+            }
+            else
+            {
+              oled_write_ln_P(led_state.caps_lock
+              ? PSTR(" -QWERTY-   SYMBOL")
+              : PSTR(" -qwerty-   symbol"), false);
+            }
+            oled_write_ln_P(led_state.caps_lock
+            ? PSTR("  MOVE      CTRL")
+            : PSTR("  move      ctrl"), false);
             break;
         case _SYMBOL :
-            oled_write_ln_P(led_state.caps_lock ? PSTR(" SYMBOL") : PSTR("symbol"), false);
+            if ( time_since_last_frame > POINTER_FRAME_DURATION)
+            {
+              oled_write_ln_P(led_state.caps_lock
+              ? PSTR("  QWERTY   >SYMBOL< ")
+              : PSTR("  qwerty   >symbol< "), false);
+            }
+            else
+            {
+              oled_write_ln_P(led_state.caps_lock
+              ? PSTR("  QWERTY   -SYMBOL-")
+              : PSTR("  qwerty   -symbol-"), false);
+            }
+            oled_write_ln_P(led_state.caps_lock
+            ? PSTR("  MOVE      CTRL")
+            : PSTR("  move      ctrl"), false);
             break;
         case _MOVE :
-            oled_write_ln_P(led_state.caps_lock ? PSTR(" MOVE") : PSTR("move"), false);
+            oled_write_ln_P(led_state.caps_lock
+            ? PSTR("  QWERTY    SYMBOL")
+            : PSTR("  qwerty    symbol"), false);
+            if ( time_since_last_frame > POINTER_FRAME_DURATION)
+            {
+              oled_write_ln_P(led_state.caps_lock
+              ? PSTR(" >MOVE<     CTRL")
+              : PSTR(" >move<     ctrl"), false);
+            }
+            else
+            {
+              oled_write_ln_P(led_state.caps_lock
+              ? PSTR(" -MOVE-     CTRL")
+              : PSTR(" -move-     ctrl"), false);
+            }
             break;
-        case _ADJUST :
-            oled_write_ln_P(led_state.caps_lock ? PSTR(" LED") : PSTR("led"), false);
+        case _CTRL :
+            oled_write_ln_P(led_state.caps_lock
+            ? PSTR("  QWERTY    SYMBOL")
+            : PSTR("  qwerty    symbol"), false);
+            if ( time_since_last_frame > POINTER_FRAME_DURATION)
+            {
+              oled_write_ln_P(led_state.caps_lock
+              ? PSTR("  MOVE     >CTRL< ")
+              : PSTR("  move     >ctrl< "), false);
+            }
+            else
+            {
+              oled_write_ln_P(led_state.caps_lock
+              ? PSTR("  MOVE     -CTRL-")
+              : PSTR("  move     -ctrl-"), false);
+            }
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
+            oled_write_P(PSTR(":Undefined:"), false);
     }
 
-    // oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
+    print_top_animation(current_frame, led_state);
 
+    // oled_write_ln(read_layer_state(), false);
+    // oled_write_ln(read_keylog(), false);
+    //oled_write_ln(read_keylogs(), false);
     //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
     //oled_write_ln(read_host_led_state(), false);
     //oled_write_ln(read_timelog(), false);
@@ -203,6 +276,7 @@ bool oled_task_user(void) {
   }
     return false;
 }
+
 #endif // OLED_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
